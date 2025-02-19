24d34a09c04677051bd121837fc9ca1fa6087e57
Sean Murphy <sean@iamseanmurphy.com>
2025-02-19 22:17:59 +0000

    5.4.0

--- pta-volunteer-sign-up-sheets/pta-volunteer-sign-up-sheets.php
+++ pta-volunteer-sign-up-sheets/pta-volunteer-sign-up-sheets.php
@@ -3,7 +3,7 @@
 Plugin Name: Volunteer Sign Up Sheets
 Plugin URI: http://wordpress.org/plugins/pta-volunteer-sign-up-sheets
 Description: Volunteer Sign Up Sheets and Management from Stephen Sherrard Plugins
-Version: 5.3.1
+Version: 5.4.0
 Author: Stephen Sherrard
 Author URI: https://stephensherrardplugins.com
 License: GPLv2 or later
@@ -20,7 +20,7 @@ if (!defined('PTA_VOLUNTEER_SUS_VERSION_KEY'))
     define('PTA_VOLUNTEER_SUS_VERSION_KEY', 'pta_volunteer_sus_version');
 
 if (!defined('PTA_VOLUNTEER_SUS_VERSION_NUM'))
-    define('PTA_VOLUNTEER_SUS_VERSION_NUM', '5.3.1');
+    define('PTA_VOLUNTEER_SUS_VERSION_NUM', '5.4.0');
 
 if (!defined('PTA_VOLUNTEER_SUS_DIR'))
 	define('PTA_VOLUNTEER_SUS_DIR', plugin_dir_path( __FILE__ ) );
@@ -61,7 +61,7 @@ class PTA_Sign_Up_Sheet {
     public function init_hooks() {
 	    add_action('pta_sus_cron_job', array($this, 'cron_functions'));
 
-	    add_action('plugins_loaded', array($this, 'init'));
+	    add_action('init', array($this, 'init'));
 	    add_action('plugins_loaded', array($this, 'public_init' ));
 
 	    add_action( 'init', array($this, 'block_assets' ));
@@ -83,7 +83,6 @@ class PTA_Sign_Up_Sheet {
 	    if (!class_exists('PTA_SUS_Public')) {
 		    include_once(dirname(__FILE__).'/classes/class-pta_sus_public.php');
 	    }
-
     }
 
     public function register_scripts() {
@@ -844,8 +843,11 @@ Please click on, or copy and paste, the link below to validate yourself:
 require_once(dirname(__FILE__).'/pta-sus-global-functions.php');
 require_once(dirname(__FILE__).'/classes/class-pta_sus_messages.php');
 require_once(dirname(__FILE__).'/classes/class-pta_sus_template_tags.php');
+require_once(dirname(__FILE__).'/classes/class-pta_sus_template_tags_helper.php');
 require_once(dirname(__FILE__).'/classes/class-pta_sus_volunteer.php');
 require_once(dirname(__FILE__).'/classes/class-pta_sus_signup_functions.php');
+require_once(dirname(__FILE__).'/classes/class-pta_sus_text_registry.php');
+
 global $pta_sus;
 $pta_sus = new PTA_Sign_Up_Sheet();
 $pta_sus->init_hooks();
--- pta-volunteer-sign-up-sheets/readme.txt
+++ pta-volunteer-sign-up-sheets/readme.txt
@@ -162,6 +162,15 @@ This alone will not make your site GDPR compliant. You will need to study up on 
 If your site is going to be affected by GDPR, then you should contact a lawyer to make sure you do everything needed to be in compliance.
 
 == Changelog ==
+**Version 5.4.0**
+*   Add a Template Tags Helper feature, and class, to help with the creation of template tags for use in email templates. Extensions can also register their own template tags with the class.
+*   The Template Tags helper will appear at the top right of admin pages where you create email templates, and allow you to quickly search template tags and copy them to the clipboard just by clicking on them. Extensions will be updated after this to add their own template tags.
+*   All the new public text strings for the validation system, as well as some previously unregistered text strings, are now registered with the new text registry class in version 4.0.0 of the Customizer extension.
+*   Fix for {contact_emails} template tag always returning "N/A".
+*   Changed the "# Needed" field on the add/edit Tasks page to be a number input instead of a text input, now that all modern browsers support number inputs.
+*   Updated and modernized the javascript for adding/removing task rows on the Add/Edit Tasks page.
+*   Tested with PHP 8.4.2 and WordPress 6.7.2
+
 **Version 5.3.1**
 *   Fixed the {validation_link} template tag for signup validation emails which was broken in 5.3.0 after separating out the email template tag functions into its own separate class.
 *   Tested with PHP 8.4.2 and WordPress 6.7.2
